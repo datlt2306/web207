fb8f73ca114a9fc92ac429c4957cf86ba7263ef2
Le Trong Dat <datlt2306@gmail.com>
2022-07-27 13:35:59 +0700

    update routing

--- chap10/controllers/product.controller.js
+++ chap10/controllers/product.controller.js
@@ -1,4 +1,4 @@
-function productController($scope) {
+function productController($scope, $routeParams) {
     $scope.productList = [
         { id: 1, name: "Product A" },
         { id: 2, name: "Product B" },
--- chap10/js/index.js
+++ chap10/js/index.js
@@ -1,12 +1,3 @@
 const app = angular.module("myApp", ["ngRoute"]);
-app.config(($routeProvider) => {
-    $routeProvider
-        .when("/product", {
-            templateUrl: "pages/product.html",
-            controller: "productController",
-        })
-        .otherwise({
-            templateUrl: "pages/other.html",
-        });
-});
+app.config(routes);
 app.controller("productController", productController);
